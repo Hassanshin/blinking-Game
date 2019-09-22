596a15bc84affcc5523be621eaa99e9c39241aa0
Hassanshin <f3fahmi@gmail.com>
2019-09-22 13:07:30 +0700

    alpha1

--- scripts/script.js
+++ scripts/script.js
@@ -7,21 +7,24 @@
 // Changelogs - https://fb.me/spark-changelog
 //==============================================================================
 
+
 // How to load in modules
 const Diagnostics = require('Diagnostics');
 const Scene = require('Scene');
 const Patches = require('Patches')
 
-Diagnostics.log(Patches);
+//Diagnostics.log(Patches);
+Diagnostics.log('start');
 
 // How to access scene objects (uncomment line below to activate)
 const v_Score = Scene.root.find('number');
-const total_Score = Patches.getStringValue('editorToScriptVar');
+//const number = Patches.getScalarValue('num');
+//const text = Patches.getStringValue('text');
+const total_Score = Patches.getScalarValue('num');
 
 // How to access class properties (uncomment line below to activate)
 v_Score.text = total_Score.toString();
 
 // How to log messages to the console (uncomment line below to activate)
-Diagnostics.log('Console message logged from the script.');
-
+//Diagnostics.log('done. ' + total_Score.toString());
 
